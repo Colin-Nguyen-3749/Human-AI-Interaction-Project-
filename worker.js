63a59a4bb5345d8f0125bfe41d366b25f899c306
Colin James Nguyen <nguyen_c9@denison.edu>
2026-05-15 05:31:41 +0000

    Get rid of test emoji commands

--- worker.js
+++ worker.js
@@ -258,7 +258,6 @@ RULES:
 - Keep summaries concise
 - Use simple language
 - ONLY USE NEWS FROM THE CURRENT YEAR (2026)
-- use emojis
 - Always add the sources along with their respective news summary, not in a bibliography all at the bottom.
 - Each source should be on a new line with a blank line beneath them.
 - Here's how I'd like you to format each of the summaries and sources:
