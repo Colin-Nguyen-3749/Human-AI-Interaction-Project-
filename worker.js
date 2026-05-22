74bd93cc88ba2570feb8e8c6586be98adc71dbe3
mxndychen <167215485+mxndychen@users.noreply.github.com>
2026-05-22 13:24:28 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -24,6 +24,7 @@ export default {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'POST, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type',
+          "Content-Type": "application/json"
         },
       });
     }
